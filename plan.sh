97098523e7f3e4320c3ab05bee7910a60feae4be
mhodzic <mhd.hodzic@gmail.com>
2020-01-03 10:53:07 -0800

    Added backend.tf. Updated plan.sh to build remote state bucket and store state in s3.

--- plan.sh
+++ plan.sh
@@ -20,9 +20,27 @@ fi
 export AWS_ACCESS_KEY_ID=$(aws configure get ${aws_profile}.aws_access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(aws configure get ${aws_profile}.aws_secret_access_key)
 
+while read line;
+do
+    export $(echo $line | tr -d "\"")
+done < ${1}
+
+echo "Creating remote state S3 bucket"
+bucket_exists=$(aws s3 ls | grep ${stack_name}-state-bucket-${region})
+if [[ -n "${bucket_exists}" ]]; then
+    echo "Bucket exists."
+  else
+    echo "Bucket does not exist or permission is not there to view it, creating bucket."
+    aws s3 mb s3://${stack_name}-state-bucket-${region} --region ${region}
+fi
+
 echo "Getting dependent tf modules"
 terraform get -update
-terraform init
+echo bucket=\""${stack_name}"-state-bucket-${region}\" > backend.conf
+echo key=\""${stack_name}/tf-infrastructure.state"\" >> backend.conf
+echo region=\""${region}"\" >> backend.conf
+
+terraform init -backend-config=backend.conf
 
 echo "Starting terraform"
 terraform plan -detailed-exitcode -refresh=true -var-file="${1}" -out=plan.out
